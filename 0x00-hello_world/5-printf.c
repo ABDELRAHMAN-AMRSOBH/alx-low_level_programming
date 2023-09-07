ba385bb78a3845c19125af75c27c03b78dc575e9
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-09-07 15:13:57 -0700

    msg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
