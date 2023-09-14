47347fb4c996e4a64b4f9b6eb87fd51ace724477
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-09-14 09:48:37 -0700

    from 0 to 7 fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
-/***/
+/**
+ * print_diagonal - prints diagonal using \
+ * @n: size
+*/
 
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i = 0, count;
 
 	while (n-- > 0)
 	{
-		while (i > 0)
+		count = i;
+		while (count-- > 0)
 			_putchar(' ');
 		_putchar('\');
 		if (n)
