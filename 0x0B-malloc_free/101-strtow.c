033b76b0ed9982ac99759fcf8a0779e2c309af20
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-10-03 12:56:53 -0700

    fixed 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,50 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * dodge_space - skip spaces
+ * @str: input
+ * Return: string without space prefix
+*/
+
+char *dodge_space(char *str)
+{
+	while (*str == ' ')
+		*str++;
+	return (str);
+}
+
+/**
+ * dodge_word - skip next word
+ * @str: string
+ * Return: string after dodged word
+*/
+
+char *dodge_word(char *str)
+{
+	while (*str != ' ' && *str != '\0')
+		*str++;
+	return (str);
+}
+
+/**
+ * get_length - get length of next word
+ * @str: string
+ * Return: length of next word + 1 for null char
+*/
+
+int get_length(char *str)
+{
+	int length = 1;
+
+	while (*str && *str != ' ')
+	{
+		length++;
+		*str++;
+	}
+	return (length);
+}
 
 /**
 * **strtow - function that splits a string into words.
@@ -9,35 +54,41 @@
 
 char **strtow(char *str)
 {
-	char **d;
-	int i;
-	int j = 0;
-	int con = 0;
+	int words = 0, word, length, index;
+	char *copy = str;
+	char **list;
 
-	if (str == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
+	while (*copy)
 	{
-		if (str[i] != 32)
-			con++;
+		if (*copy != ' ')
+		{
+			words++;
+			copy = dodge_word(copy);
+		}
+		copy = dodge_space(copy);
 	}
-
-	d = malloc(sizeof(char) * con);
-
-	if (d == NULL)
+	list = malloc(sizeof(list) * words);
+	if (list == NULL)
 		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (word = 0; word < words; word++)
 	{
-		if (str[i] != 32)
+		index = 0;
+		str = dodge_space(str);
+		length = get_length(str);
+		list[word] = malloc(sizeof(char) * length);
+		if (list[word] == NULL)
 		{
-			*d[j] = str[i];
-			j++;
+			for (word = word - 1; word >= 0; word--)
+				free(list[word]);
+			free(list);
+			return (NULL);
 		}
-		else
-		{
-		}
+		while (*str != ' ' && *str != '\0')
+			list[word][index++] = *str++;
+		list[word][index] = '\0';
 	}
-	return (d);
+	list[word] = NULL;
+	return (list);
 }
