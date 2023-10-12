962379c1834d739ecdc316fc4e30596307810435
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-10-12 07:11:51 -0700

    solution for 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print n numbers separated by a separator
+ * @separator: exist between each 2 numbers
+ * @n: count of incoming arguments
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int cp = n;
+
+	va_start(ap, n);
+	while (cp--)
+	{
+		printf("%d%s", va_arg(ap, int), cp ? separator : "\n");
+		va_end(ap);
+	}
+}
