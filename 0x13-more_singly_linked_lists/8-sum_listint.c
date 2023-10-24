bbbf85982d0ef1fffaeab2f6a6bd8fe32b7a226f
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-10-24 10:04:21 -0700

    msg

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum the n in nodes
+ * @head: list of nodes
+ * Return: sum
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head;
+		head = head->next;
+	}
+	return (sum);
+}
