f5f113c09a47a508326f7cd95cfa4b06d379f9a8
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-10-24 09:51:29 -0700

    msg

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - give n with head and free node
+ * @head: list
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int value = 0;
+
+	if (head && *head)
+	{
+		node = *node;
+		*head = node->next;
+		value = node->n;
+		free(node);
+	}
+	return (value);
+}
