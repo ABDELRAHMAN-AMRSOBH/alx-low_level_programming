d54b1f3f47e9cf302d2eb9d3ffc3263093cd59b3
Abdelrahman <Abdelrahmanamr.egypt@gmail.com>
2023-10-30 12:10:10 -0700

    msg

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text to file
+ * @filename: name of file
+ * @text_content: text to append
+ * Return: 1 on success -1 on failure
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, size = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	while (text_content[size])
+		size++;
+	write(fd, text_content, size);
+	return (1);
+}
