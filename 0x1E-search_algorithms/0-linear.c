4de64572a0b973b02816bbf212cdf061c0cd4ed3
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-07-15 10:57:27 +0300

    Solve tasks

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - implemention of linear search
+ * @array: list of numbers
+ * @size: size of the list
+ * @value: targeted value
+ * Return: index if found else -1
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+        int i = 0;
+
+        while (i < (int) size)
+        {
+                printf("Value checked array[%d] = [%d]\n", i, array[i]);
+                if (array[i] == value)
+                        return (i);
+                i++;
+        }
+        return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,5 @@
+# README
+
+-- Written by Abdelrahman Amr
+
+# implemention of linear search and binary search along with Q/A about time & space complexity
