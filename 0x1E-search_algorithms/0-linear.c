422a41c5f0df3581edc674d40cbd8be28a6e3d42
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-07-16 14:52:58 +0300

    Fix betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,14 +10,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-        int i = 0;
+	int i = 0;
 
-        while (i < (int) size)
-        {
-                printf("Value checked array[%d] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
-                i++;
-        }
-        return (-1);
+	while (i < (int) size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,29 +10,29 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-        int left = 0, right = size - 1;
-        int middle = left + (right - left) / 2;
-        int index = 0;
+	int left = 0, right = size - 1;
+	int middle = left + (right - left) / 2;
+	int index = 0;
 
-        while (left <= right)
-        {
-                middle = left + (right - left) / 2;
-                printf("Searching in array: ");
-                index = left;
-                while (index <= right)
-                {
-                        if (index == right)
-                                printf("%d\n", array[index]);
-                        else
-                                printf("%d, ", array[index]);
-                        index++;
-                }
-                if (array[middle] == value)
-                        return (middle);
-                else if (array[middle] > value)
-                        right = middle - 1;
-                else
-                        left = middle + 1;
-        }
-        return (-1);
+	while (left <= right)
+	{
+		middle = left + (right - left) / 2;
+		printf("Searching in array: ");
+		index = left;
+		while (index <= right)
+		{
+			if (index == right)
+				printf("%d\n", array[index]);
+			else
+				printf("%d, ", array[index]);
+			index++;
+		}
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] > value)
+			right = middle - 1;
+		else
+			left = middle + 1;
+	}
+	return (-1);
 }
